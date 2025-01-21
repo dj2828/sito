5764f033fe86fb1f7f05d30d75391e0f34223379
dj 2828 <dj28partystudio@gmail.com>
2025-01-21 16:30:54 +0100

    help kick

---
+++ dj help.js
@@ -0,0 +1,54 @@
+const webhookUrl = 'https://discord.com/api/webhooks/1331284746663362652/jB_5kd9v5KHLSh3vgyQ8hvFo-km8b_NYdiDuWNE1avYvZ68CAerpT4voHA5UN-PJ3HA3'; // Inserisci qui l'URL del tuo webhook
+
+function help() {
+    // Ottieni il nome dall'input dell'utente
+    const userName = document.getElementById('userName').value;
+    const why = document.getElementById('why').value;
+    const dove = document.getElementById('dove').value;
+
+    // Verifica che l'input non sia vuoto
+    if (!userName || !why) {
+        alert('Per favore inserisci un nome e/o probblema');
+        return; // Esce dalla funzione se l'input è vuoto
+    }
+
+    // Dati da inviare al webhook
+    const data = {
+        content: '<@685136676984586260> '+userName+' // `'+why+'` // '+dove
+    }
+    invio(data);
+}
+function kick(){
+    const userName = document.getElementById('userName').value;
+
+    if (!userName) {
+        alert('Per favore inserisci un nome');
+        return; // Esce dalla funzione se l'input è vuoto
+    }
+
+    const data = {
+        content: '<@685136676984586260> ' + userName + ' ha chiesto aiuto per il kick'
+    }
+    invio(data);
+}
+
+function invio(data){
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            // alert('inviato con successo!');
+        } else {
+            alert('Errore nell\'invio del webhook. Status: ' + response.status);
+        }
+    })
+    .catch(error => {
+        console.error('Errore:', error);
+        alert('Errore di rete durante l\'invio del webhook.');
+    })
+}
