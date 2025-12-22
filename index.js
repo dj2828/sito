1daa6a3332110c41515ddf83aec6b26a36e2b833
dj 2828 <dj28partystudio@gmail.com>
2025-12-22 11:36:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ const redirectMap = {
     'admin': 'http://dj2828.ga/script/admin.html',
     'pc': 'https://dj2828.github.io/sito/PC!!!.html',
     '1c1': 'https://dj2828.github.io/1C1',
-    'server': 'https://github.com/dj2828/sito/releases/download/mod_vanilla_2/mod-server.py',
+    'server': 'https://github.com/dj2828/Ultra_Vanilla_2/blob/main/download_mod/server/mod-server-down.py',
     'w': 'https://raw.githubusercontent.com/dj2828/aggiorna-dj/main/down/watchdog.zip'
 };
 
